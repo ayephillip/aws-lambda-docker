21c11a1f1ef718b244abd4d74db916883d428e52
ayephillip <phillip@unwritten.co>
2024-06-09 18:28:50 +0100

    customized json msg

--- image/src/main.py
+++ image/src/main.py
@@ -5,5 +5,5 @@ def handler(event, context):
     arr = np.random.randint(0, 10, (3, 3))
     return {
         "statusCode": 200,
-        "body": {"message": "Hello from Lambda!", "array": arr.tolist()},
+        "body": {"message": "Hello, Phillip from Lambda!", "array": arr.tolist()},
     }
